e69ffb0b048428e2fdd44f7ff14779db0afb74fd
coronaroman3603-ctrl <coronaroman3603@gmail.com>
2026-02-28 04:38:59 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "AI agent server is running" }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
